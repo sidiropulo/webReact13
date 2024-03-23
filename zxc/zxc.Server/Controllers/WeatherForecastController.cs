768add9a9c961daa5e260d8c9a5757cad086e655
sidiropulo <ximeraproject@gmail.com>
2024-03-23 17:46:38 +0300

    async запрос

--- zxc/zxc.Server/Controllers/WeatherForecastController.cs
+++ zxc/zxc.Server/Controllers/WeatherForecastController.cs
@@ -19,15 +19,19 @@ namespace zxc.Server.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async IAsyncEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var rng = new Random();
+            foreach (var index in Enumerable.Range(1, 5))
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                yield return new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                };
+                await Task.Delay(1000); // Задержка в 1 секунду между обновлениями
+            }
         }
     }
 }
